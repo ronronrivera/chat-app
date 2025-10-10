93a8ca66712bf4ee30d5cad036e258a12d40dc48
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2025-10-10 20:19:47 +0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
+
+
 const app = express();
 dotenv.config();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+//make ready for deployment
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.get("*", (req, res) =>{
+		res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+	});
+}
 
-app.listen(PORT, () => console.log(`Sever is running at port ${PORT}`));
+app.listen(PORT, () =>	 console.log(`Sever is running at port ${PORT}`));
