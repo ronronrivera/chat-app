16fad98b7b8799b1e4e69af69d77ecf85f7f174e
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2025-10-11 19:58:51 +0800

    signup controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,65 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+
+export const Signup = async (req, res) =>{
+
+	const {fullname, email, password} = req.body;
+
+	try{
+		if(!fullname || !email || !password){
+			return res.status(400).json({message: "All fields are required"})
+		}
+		else if(password.length < 8){
+			return res.status(400).json({message: "Password must be at least 8 characters"})
+		}
+		//check if email is valid with regex;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+		const user = await User.findOne({email});
+
+		if(user) return res.status(400).json({message: "Email already exist"});
+	
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			fullname,
+			password: hashedPassword,
+			email,
+		})
+	
+		if(newUser){
+			generateToken(newUser._id, res);
+			await newUser.save();
+
+			res.status(201).json({
+				_id: newUser._id,
+				fullname: newUser.fullname,
+				email: newUser.email,
+				profilePic: newUser.profilePic
+			})
+		}
+		else{
+			return res.status(400).json({message: "Invalid user data"});
+		}
+
+	}
+	catch(error){
+		console.log("Error in signup controller: ", error);
+		res.status(500).json({message: "Internal server error"})
+	}
+
+}
+/*
+export const Login =  (req, res) =>{
+	res.send('login end point');
+}
+
+export const Logout =  (req, res) =>{
+res.send('logout end point');
+}
+*/
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
-
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 dotenv.config();
@@ -13,8 +13,11 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); //req.body
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
 
 //make ready for deployment
 if(process.env.NODE_ENV === "production"){
@@ -24,4 +27,7 @@ if(process.env.NODE_ENV === "production"){
 	});
 }
 
-app.listen(PORT, () =>	 console.log(`Sever is running at port ${PORT}`));
+app.listen(PORT, () =>	 {	
+	console.log(`Sever is running at port ${PORT}`)
+	connectDB();
+});
