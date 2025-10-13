eda76c718970e4d45cc529f52c191f86f8670a24
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2025-10-13 13:57:13 +0800

    implemented rate limiter

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { Signup, Login, Logout, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
+
 router.post('/signup', Signup);
-router.post('/login',Login);
+router.post('/login',arcjetProtection,Login);
 router.post('/logout',Logout);
 
 router.put('/update-profile', protectRoute, updateProfile);
