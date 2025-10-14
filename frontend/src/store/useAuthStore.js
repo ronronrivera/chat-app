59b6571c5029b84ceb333ff7c9dc4e29ce4cf719
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2025-10-14 18:50:53 +0800

    signup implemented on frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,41 @@
 import {create} from "zustand"
-
+import {axiosInstance} from "../lib/axios.js";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-  authUser: {name: "John", _id:123, age:25},
-  isLoading: false,
 
-  login: ()=>{
-    console.log("We just logged in");
+  authUser: null,
+  isCheckingAuth: true,
+  isSignUp: false,
+
+  checkAuth: async () =>{
+    try{
+      const res = await  axiosInstance.get("/auth/check");
+      set({authUser: res.data});
+    }
+    catch(error){
+      console.log("Error in authCheck", error);
+      set({authUser: null});
+    }
+    finally{
+      set({isCheckingAuth: false});
+    }
   },
-}))
+  
+  signup: async (data) =>{
+    set({isSignUp: true});
+    try{
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({authUser: res.data}); 
+
+      toast.success("Account created successfully!");
+    }
+    catch(error){
+      toast.error(error.response.data.message);
+    }
+    finally{
+      set({isSignUp: false});
+    }
+  }
+
+})) 
